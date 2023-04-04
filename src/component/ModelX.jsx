7437bfa4d30d587ab3a2e2cde4dd61f30bb04dd1
debanjo31 <debanjo31@gmail.com>
2023-04-04 19:15:36 +0100

    working on the button

--- src/component/ModelX.jsx
+++ src/component/ModelX.jsx
@@ -7,14 +7,14 @@ const ModelX = () => {
         <div className='relative w-full h-screen bg-blue-500 '>
             <img src={model3} className="h-full w-full object-cover" alt="" />
         </div>
-        <div className='absolute top-0 left-0 w-full flex flex-col gap-36 items-center justify-around h-screen text-center'>
+        <div className='absolute top-0 left-0 w-full flex flex-col gap-24 items-center justify-around h-screen text-center'>
             <div>
                <h1 className=' text-center text-5xl '>Model 3</h1>
                <p>Leading starting at $349/mo</p>
             </div>
             <div className='flex flex-col w-full gap-2'>
-                <button className='bg-black  w-5/6 mx-auto py-1 text-white'>View Inventory</button>
-                <button className='bg-gray-100  w-5/6 mx-auto py-1 text-black font-bold'>Custom Order</button>
+                <button className='bg-gray-800  w-5/6 mx-auto py-2 text-white rounded-md'>View Inventory</button>
+                <button className='bg-gray-100  w-5/6 mx-auto py-2 text-black font-bold rounded-md'>Custom Order</button>
             </div>
         </div>
     </div>
