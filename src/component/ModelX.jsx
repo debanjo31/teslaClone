5ba3e804df7632926ed701f4a58740cecd927a74
debanjo31 <debanjo31@gmail.com>
2023-04-04 02:25:35 +0100

    finding right font

--- src/component/ModelX.jsx
+++ src/component/ModelX.jsx
@@ -7,8 +7,12 @@ const ModelX = () => {
         <div className='relative w-full h-screen bg-blue-500'>
             <img src={model3} className="h-full w-full object-cover" alt="" />
         </div>
-        <div className='absolute top-0 left-0 w-full flex flex-col items-center justify-around h-screen text-center'>
-            <h1 className=' text-center text-5xl  '>Model 3</h1>
+        <div className='absolute top-0 left-0 w-full flex flex-col gap-8 items-center justify-around h-screen text-center'>
+            <div>
+               <h1 className=' text-center text-5xl '>Model 3</h1>
+               <p>Leading starting at $349/mo</p>
+            </div>
+            
             <h1 className=' text-center'>Model 3</h1>
         </div>
     </div>
