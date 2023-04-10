c5c14b227acea2f54180b468cc8809c09378a06b
debanjo31 <debanjo31@gmail.com>
2023-04-10 14:40:28 +0100

    deleting

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -1,41 +1,34 @@
 import React from 'react'
-import Model from './Model'
 import NavBar from './NavBar'
+import Model from './Model'
+// import model3 from '../assets/model3.jpg'
+// import modelY from '../assets/modelY.jpg'
 import model3 from '../assets/model3.jpg'
-import modelY from '../assets/modelY.jpg'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import "swiper/css/pagination";
-import { Pagination } from "swiper";
 const Homepage = () => {
   return (
     <div className=''>
-        <div className='relative'>
-              <Swiper
-            direction={"vertical"}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-          >
-              <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-            {/* <SwiperSlide> <Model img={model3} model="Model 3" text="Leasing starting at $349/mo" /></SwiperSlide>
-            <SwiperSlide>  <Model img={modelY} model="Model Y" text="Schedule a Demo Drive" className="relative" /></SwiperSlide>
-            <SwiperSlide> <Model img={model3} model="Model 3" text="Leasing starting at $349/mo" /></SwiperSlide>
-            <SwiperSlide>  <Model img={modelY} model="Model Y" text="Schedule a Demo Drive" className="relative" /></SwiperSlide> */}
-          </Swiper>
-         
-        
-        </div>
+         <div className='relative'>
+          {/* <Model  model="Model 3" text="Leasing starting at $349/mo" />
+           {/* <Model img={model3} model="Model 3" text="Leasing starting at $349/mo" />
+            <Model img={modelY} model="Model Y" text="Schedule a Demo Drive" className="relative" />
+           <Model img={model3} model="Model 3" text="Leasing starting at $349/mo" />
+            <Model img={modelY} model="Model Y" text="Schedule a Demo Drive" className="relative" />  */}
+            <div>
+              <div className='relative w-full h-screen bg-blue-500 '>
+                  <img src={model3} className="h-full w-full object-cover" alt="" />
+              </div>
+              <div className='absolute top-0 left-0 w-full flex flex-col gap-24 items-center justify-around h-screen text-center'>
+                  <div>
+                      <h1 className=' text-center text-5xl '> Model Y</h1>
+                      <p>NEXT MODEL</p>
+                  </div>
+                  <div className='flex flex-col w-full gap-2'>
+                      <button className='bg-gray-800  w-5/6 mx-auto py-2 text-white rounded-md'>View Inventory</button>
+                      <button className='bg-gray-100  w-5/6 mx-auto py-2 text-black font-bold rounded-md'>Custom Order</button>
+                  </div>
+              </div>
+          </div>        
+        </div> 
        <div className='fixed top-0 left-0 w-full'>
             <NavBar />
        </div>
--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import model3 from '../assets/model3.jpg'
 
 
-const Model = ({model, text, img}) => {
+const Model = ({model, text, }) => {
   return (
     <div>
         <div className='relative w-full h-screen bg-blue-500 '>
-            <img src={img} className="h-full w-full object-cover" alt="" />
+            <img src={model3} className="h-full w-full object-cover" alt="" />
         </div>
         <div className='absolute top-0 left-0 w-full flex flex-col gap-24 items-center justify-around h-screen text-center'>
             <div>
