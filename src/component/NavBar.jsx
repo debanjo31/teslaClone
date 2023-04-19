6b5e89f6050528ae81b00de422044612b6f15d34
debanjo31 <debanjo31@gmail.com>
2023-04-19 06:00:47 +0100

    working on navBar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -4,13 +4,14 @@ import tesla from '../assets/tesla.svg'
 
 const NavBar = () => {
   const [status, setStatus] = useState(false);
+ 
   function Sidebar() {
     return (
       <div className='fixed top-0 left-0 w-full h-screen flex overflow-hidden z-30'>
           <div className='w-2/6 bg-gray-600 bg-opacity-75 ' onClick={()=> setStatus(false)}></div>
           <div className='w-4/6 bg-white p-4'>
                <p onClick={()=> setStatus(false)} className='block cursor-pointer text-xl text-right'>X</p>
-               <div className='flex flex-col gap-16 overflow-auto'>
+               <div className='flex flex-col gap-16 overflow-auto h-screen'>
                   <p>Model S</p>
                   <p>Model 3</p>
                   <p>Model X</p>
